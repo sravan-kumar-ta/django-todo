c1a127b76a510b1d70c21466a4930459b181d364
sravan-kumar-ta <sravan0095@gmail.com>
2021-11-25 17:19:15 +0530

    app and database created

---
+++ todo_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import datetime
+
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    priority = models.CharField(max_length=20)
+    created_at = models.DateField(default=datetime.date.today)
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
